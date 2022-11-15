41c84c3f6d4abb3d0c425cb7bc0d12568cada882
Mateo Westphal <mateojwestphal@gmail.com>
2022-11-15 11:51:25 +0000

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def touch
+  end
+end
+
+# Rails.application.routes.draw do
+  # get "/ask", to: "ask#index"
+
+
+# end
+
+# verb "url", to: "controller#action"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/app/controllers/questions_controller.rb', '/app/views/layouts/application.html.erb'
   # Defines the root path route ("/")
   # root "articles#index"
 end
